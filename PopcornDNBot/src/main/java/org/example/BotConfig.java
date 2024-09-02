232f0db985e88dcf3440c2f2bc2f6bf114d279de
Nevhadini1 <62599376+Nevhadini1@users.noreply.github.com>
2024-09-02 20:15:53 +0300

    Delete PopcornDNBot directory

--- PopcornDNBot/src/main/java/org/example/BotConfig.java
+++
@@ -1,6 +0,0 @@
-package org.example;
-
-public class BotConfig {
-    public static final String BOT_TOKEN = "7121682508:AAEF2Jkwiqr33LlUm-H3FS4ZDk7CyAgTv0k";
-    public static final String BOT_USERNAME = "PopcornDNBot";
-}
