f92343b57d0d10275d96732ca3e0d8416c99cafc
Nevhadini1 <62599376+Nevhadini1@users.noreply.github.com>
2024-09-02 13:57:56 +0300

    Add files via upload

---
+++ PopcornDNBot/src/main/java/org/example/Movie.java
@@ -0,0 +1,79 @@
+package org.example;
+
+import java.util.List;
+
+public class Movie {
+    private int id;
+    private String title;
+    private double rating;
+    private List<String> genres;
+    private String overview;
+    private String posterPath;
+
+    private String year;
+    private String trailerUrl;
+
+    public String getTrailerUrl() {
+        return trailerUrl;
+    }
+
+    public void setTrailerUrl(String trailerUrl) {
+        this.trailerUrl = trailerUrl;
+    }
+
+    public String getYear() {
+        return year;
+    }
+
+    public void setYear(String year) {
+        this.year = year;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public double getRating() {
+        return rating;
+    }
+
+    public void setRating(double rating) {
+        this.rating = rating;
+    }
+
+    public List<String> getGenres() {
+        return genres;
+    }
+
+    public void setGenres(List<String> genres) {
+        this.genres = genres;
+    }
+
+    public String getOverview() {
+        return overview;
+    }
+
+    public void setOverview(String overview) {
+        this.overview = overview;
+    }
+
+    public String getPosterPath() {
+        return posterPath;
+    }
+
+    public void setPosterPath(String posterPath) {
+        this.posterPath = posterPath;
+    }
+}
